4295d4bd4b39651a2ac680dd2c6446229352daf3
Platon Neutron <mr.celioofficiel@gmail.com>
2023-01-26 18:03:16 +0100

    Code Clarification

--- SkyMain.py
+++ SkyMain.py
@@ -2,8 +2,6 @@ import discord
 from discord import app_commands
 
 intents = discord.Intents.default()
-intents.message_content = True
-
 bot = discord.Client(intents=intents)
 base = app_commands.CommandTree(bot)
 
@@ -12,21 +10,26 @@ base = app_commands.CommandTree(bot)
 @bot.event
 async def on_ready():
     await base.sync(guild=discord.Object(id=362272596517191691))
-    print(f'We have logged in as {bot.user}')
+    print(f'{bot.user} has successfuly connected to Discord')
 
 #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.                                 
-@base.command(name = "config", description = "All the configs of the bots", guild=discord.Object(id=362272596517191691))
+@base.command(name = "config_wip", description = "To config the bot", guild=discord.Object(id=362272596517191691))
 async def first_command(interaction):
-    await interaction.response.send_message("... World!")
+    await interaction.response.send_message("WIP")
 
 @base.command(name = "shutdown", description = "Shutdown the bot", guild=discord.Object(id=362272596517191691)) 
 async def shutdown(interaction):
     if interaction.user.id == 286827468445319168:
+        print(f'{bot.user} has been successfuly shutdown')
         await bot.close()
 
 # Skyblock commands part
-@base.command(name = "hello", description = "My first application Command", guild=discord.Object(id=362272596517191691))
+@base.command(name = "évènements_wip", description = "A list of all the events this week", guild=discord.Object(id=362272596517191691))
 async def first_command(interaction):
-    await interaction.response.send_message("... World!")
+    await interaction.response.send_message("WIP")
+
+@base.command(name = "maire_wip", description = "To see the current mayor and perks", guild=discord.Object(id=362272596517191691))
+async def first_command(interaction):
+    await interaction.response.send_message("WIP")
 
 bot.run('')
