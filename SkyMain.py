4c261221eb3c878f60ff1e74d35b8e997c6235ef
Platon Neutron <mr.celioofficiel@gmail.com>
2023-01-27 14:51:55 +0100

    First version of the mayor/election command

--- SkyMain.py
+++ SkyMain.py
@@ -1,9 +1,12 @@
+import requests
+
 import discord
 from discord import app_commands
 
 intents = discord.Intents.default()
 bot = discord.Client(intents=intents)
 base = app_commands.CommandTree(bot)
+hypixelApiOAuth = {'Authorization' : 'Bearer {1f49f071-cf58-4f77-a710-5f4a8ae06c7a}'}
 
 
 # Main part of the bot
@@ -30,6 +33,35 @@ async def events(interaction):
 
 @base.command(name = "mayor_wip", description = "To see the current mayor and perks", guild=discord.Object(id=362272596517191691))
 async def mayor(interaction):
-    await interaction.response.send_message("WIP")
+    hypixelApiRequest = requests.get('https://api.hypixel.net/resources/skyblock/election', headers=hypixelApiOAuth)
+    request = hypixelApiRequest.json()
+    getCurrentMayor = request["mayor"]["name"]
 
-bot.run('')
+    electionEmbed = discord.Embed(title="Test", description="test")
+
+    if(getCurrentMayor == "Aatrox"):
+        electionEmbed.set_thumbnail(url="https://raw.githubusercontent.com/PlatonNeutron/SkyBot/main/assets/mayor/Aatrox_Sprite.webp")
+    elif(getCurrentMayor == "Cole"):
+        electionEmbed.set_thumbnail(url="https://raw.githubusercontent.com/PlatonNeutron/SkyBot/main/assets/mayor/Cole_Sprite.webp")
+    elif(getCurrentMayor == "Derpy"):
+        electionEmbed.set_thumbnail(url="https://raw.githubusercontent.com/PlatonNeutron/SkyBot/main/assets/mayor/Derpy_Sprite.webp")
+    elif(getCurrentMayor == "Diana"):
+        electionEmbed.set_thumbnail(url="https://raw.githubusercontent.com/PlatonNeutron/SkyBot/main/assets/mayor/Diana_Sprite.webp")
+    elif(getCurrentMayor == "Diaz"):
+        electionEmbed.set_thumbnail(url="https://raw.githubusercontent.com/PlatonNeutron/SkyBot/main/assets/mayor/Diaz_Sprite.webp")
+    elif(getCurrentMayor == "Finnegan"):
+        electionEmbed.set_thumbnail(url="https://raw.githubusercontent.com/PlatonNeutron/SkyBot/main/assets/mayor/Finnegan_Sprite.webp")
+    elif(getCurrentMayor == "Foxy"):
+        electionEmbed.set_thumbnail(url="https://raw.githubusercontent.com/PlatonNeutron/SkyBot/main/assets/mayor/Foxy_Sprite.webp")
+    elif(getCurrentMayor == "Jerry"):
+        electionEmbed.set_thumbnail(url="https://raw.githubusercontent.com/PlatonNeutron/SkyBot/main/assets/mayor/Jerry_Sprite.webp")
+    elif(getCurrentMayor == "Marina"):
+        electionEmbed.set_thumbnail(url="https://raw.githubusercontent.com/PlatonNeutron/SkyBot/main/assets/mayor/Marina_Sprite.webp")
+    elif(getCurrentMayor == "Paul"):
+        electionEmbed.set_thumbnail(url="https://raw.githubusercontent.com/PlatonNeutron/SkyBot/main/assets/mayor/Paul_Sprite.webp")
+    else:
+        electionEmbed.set_thumbnail(url="https://raw.githubusercontent.com/PlatonNeutron/SkyBot/main/assets/mayor/Scorpius_Sprite.webp")
+    
+    await interaction.response.send_message(embed=electionEmbed)
+
+bot.run('MTA2ODEyOTIzNzU5MDA5Mzg5NA.GnSfiA.KApS_OU3v729tiwvKn1-NBrAjQasyw6eRKHi7g')
